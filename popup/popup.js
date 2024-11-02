3ac1447fe7b88558d6a9d5fd44f4724512622f8e
Gradyn Wursten <me@gradyn.com>
2024-11-01 18:36:09 -0600

    initial commit

---
+++ popup/popup.js
@@ -0,0 +1,17 @@
+const main = async () => {
+    let filters = await storage.local.get("filters");
+    let filter = null;
+}
+
+/**
+ * @param {string} url - The URL
+ * @param {number} filter - The filter
+ * @returns {boolean} Rather or not the URL matches the filter
+ */
+const checkUrlAgainstFilter = (url, filter) => {
+    for (i = 0; i < filter.length; i++) {
+
+    }
+}
+
+main();
